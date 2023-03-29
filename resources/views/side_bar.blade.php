3ac66a9a49a42fb45e16f14c61048aebf3b07053
dustin <dustin.chang@lioshutan.com>
2023-03-30 01:58:26 +0800

    調整背景 sidebar

--- resources/views/side_bar.blade.php
+++ resources/views/side_bar.blade.php
@@ -2,12 +2,12 @@
 
 @section('sidebar')
     <div class="sidebar">
-        <h3 class="neon" style="text-align: center">單次提醒</h3>
+        <h2 class="sidebar-text">單次提醒</h2>
         <ul>
             <li><a href="{{ env('APP_URL') }}/memo">建立提醒</a></li>
             <li><a href="{{ env('APP_URL') }}/memos">提醒紀錄</a></li>
         </ul>
-        <h3 class="neon" style="text-align: center">重複提醒</h3>
+        <h2 class="sidebar-text">重複提醒</h2>
         <ul>
             <li><a href="{{ env('APP_URL') }}/regular-event">建立定期</a></li>
             <li><a href="{{ env('APP_URL') }}/regular-events">定期紀錄</a></li>
